9481d9e00729f949415da7989f170f24ae6d0588
Charles <1309571292@qq.com>
2020-04-03 16:09:44 +0800

    使用url.parse来处理查询参数

--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
 import { IncomingMessage, ServerResponse } from "http";
 import * as http from "http";
+import * as url from "url";
 const api = require("./api.js");
 const server = http.createServer();
 
 server.on("request", (request: IncomingMessage, response: ServerResponse) => {
-  const { url } = request;
-  switch (url) {
+  const { url: path } = request;
+  const pathObject = url.parse(path);
+  const { pathname } = pathObject;
+  switch (pathname) {
     case "/index.html":
       // __dirname： 当前文件所在目录
       api.setContentType("text/html", response);
